a401f94ca4dfc85e7dee58fdf182ff39f0401fe4
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-04-24 17:52:04 -0600

    add beginning essay portions

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
 // - - - - - - - - - QUERY SELECTORS - - - - - - - - - - //
+const buttonViewPage = document.querySelector(".center");
 const button = document.querySelector(".button");
+const essayViewPage = document.querySelector(".essay-view-page");
+const articleSection = document.querySelector(".article-section");
+const essay = document.querySelector(".essay");
 
 // - - - - - - - - - - EVENT LISTENERS - - - - - - - - - - //
-button.addEventListener("click", goToEssayPage());
+button.addEventListener("click", (e) => goToEssayOnePage());
 
 // - - - - - - - - - - FUNCTIONS - - - - - - - - - - //
-const goToEssayPage = () => {};
+
+const goToEssayOnePage = () => {
+  hideElement([buttonViewPage]);
+  showElement([essayViewPage, articleSection]);
+};
+
+const showElement = (domItems) => {
+  domItems.forEach((domItem) => {
+    domItem.classList.remove("hidden");
+  });
+};
+
+const hideElement = (domItems) => {
+  domItems.forEach((domItem) => {
+    domItem.classList.add("hidden");
+  });
+};
