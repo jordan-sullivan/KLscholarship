76bf3ad3950552abc14e8845fa5984bebe121aba
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-04-24 18:08:16 -0600

    add back and forward buttons and styling

--- scripts.js
+++ scripts.js
@@ -4,13 +4,38 @@ const button = document.querySelector(".button");
 const essayViewPage = document.querySelector(".essay-view-page");
 const articleSection = document.querySelector(".article-section");
 const essay = document.querySelector(".essay");
+const footer = document.querySelector(".nav-buttons-footer");
 
 // - - - - - - - - - - EVENT LISTENERS - - - - - - - - - - //
 button.addEventListener("click", (e) => goToEssayOnePage());
+button.addEventListener("click", (e) => goToEssayTwoPage());
+button.addEventListener("click", (e) => goToEssayThreePage());
+button.addEventListener("click", (e) => goToEssayFourPage());
+button.addEventListener("click", (e) => goToEssayFivePage());
 
 // - - - - - - - - - - FUNCTIONS - - - - - - - - - - //
 
 const goToEssayOnePage = () => {
+  hideElement([buttonViewPage]);
+  showElement([essayViewPage, articleSection]);
+};
+
+const goToEssayTwoPage = () => {
+  hideElement([buttonViewPage]);
+  showElement([essayViewPage, articleSection]);
+};
+
+const goToEssayThreePage = () => {
+  hideElement([buttonViewPage]);
+  showElement([essayViewPage, articleSection]);
+};
+
+const goToEssayFourPage = () => {
+  hideElement([buttonViewPage]);
+  showElement([essayViewPage, articleSection]);
+};
+
+const goToEssayFivePage = () => {
   hideElement([buttonViewPage]);
   showElement([essayViewPage, articleSection]);
 };
