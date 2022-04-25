1b32cc95badf374878460036f8379758672d0b2b
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-04-24 19:21:06 -0600

    add more pages to essay

--- scripts.js
+++ scripts.js
@@ -2,18 +2,21 @@
 const buttonViewPage = document.querySelector(".center");
 const button = document.querySelector(".button");
 const essayOneViewPage = document.querySelector(".essay-one-view-page");
+const firstButton = document.querySelector(".first-button");
+const essayTwoViewPage = document.querySelector(".essay-two-view-page");
+const secondButton = document.querySelector(".second-button");
+const essayThreeViewPage = document.querySelector(".essay-three-view-page");
+const thirdButton = document.querySelector(".third-button");
 const articleSection = document.querySelector(".article-section");
 const essay = document.querySelector(".essay");
 const footer = document.querySelector(".nav-buttons-footer");
-const secondButton = document.querySelector(".forward");
-const thirdButton = document.querySelector(".back");
 
 // - - - - - - - - - - EVENT LISTENERS - - - - - - - - - - //
 button.addEventListener("click", (e) => goToEssayOnePage());
-button.addEventListener("click", (e) => goToEssayTwoPage());
-button.addEventListener("click", (e) => goToEssayThreePage());
-button.addEventListener("click", (e) => goToEssayFourPage());
-button.addEventListener("click", (e) => goToEssayFivePage());
+firstButton.addEventListener("click", (e) => goToEssayTwoPage());
+secondButton.addEventListener("click", (e) => goToEssayThreePage());
+// button.addEventListener("click", (e) => goToEssayFourPage());
+// button.addEventListener("click", (e) => goToEssayFivePage());
 
 // - - - - - - - - - - FUNCTIONS - - - - - - - - - - //
 
@@ -23,24 +26,24 @@ const goToEssayOnePage = () => {
 };
 
 const goToEssayTwoPage = () => {
-  hideElement([buttonViewPage]);
-  showElement([essayViewPage, articleSection]);
+  hideElement([buttonViewPage, essayOneViewPage]);
+  showElement([essayTwoViewPage, articleSection]);
 };
 
 const goToEssayThreePage = () => {
-  hideElement([buttonViewPage]);
-  showElement([essayViewPage, articleSection]);
+  hideElement([buttonViewPage, essayOneViewPage, essayTwoViewPage]);
+  showElement([essayThreeViewPage, articleSection]);
 };
-
-const goToEssayFourPage = () => {
-  hideElement([buttonViewPage]);
-  showElement([essayViewPage, articleSection]);
-};
-
-const goToEssayFivePage = () => {
-  hideElement([buttonViewPage]);
-  showElement([essayViewPage, articleSection]);
-};
+//
+// const goToEssayFourPage = () => {
+//   hideElement([buttonViewPage]);
+//   showElement([essayViewPage, articleSection]);
+// };
+//
+// const goToEssayFivePage = () => {
+//   hideElement([buttonViewPage]);
+//   showElement([essayViewPage, articleSection]);
+//};
 
 const showElement = (domItems) => {
   domItems.forEach((domItem) => {
